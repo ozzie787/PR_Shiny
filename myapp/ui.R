88c5adbdbc5faeb92d0eca2c2c51514a8e23f29c
ozzie787 <ozzie787@hotmail.co.uk>
2018-09-21 00:24:47 +0100

    backup commit

--- myapp/ui.R
+++ myapp/ui.R
@@ -1,30 +1,36 @@
 library(shiny)
+library(shiny)
+library(datasets)
+library(ggplot2)
+library(plotly)
+data("cars")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("My App"),
+  titlePanel("Stopping distance of a 1920's Car"),
   
-  # Sidebar with a slider input for number of bins 
+  # Sidebar with a slider input for car speed 
   sidebarLayout(
     sidebarPanel(
          sliderInput("xint","Speed of 1920's Car (miles/hour):", min=0, max=30, value=15, step=0.1),
-         sliderInput("ptalpha","%Point ocapacity:", min=0, max=100, value=50, step=1),
-         sliderInput("lnwt","Graph line width", min=0, max=5, value=1, step=0.1),
-         checkboxInput("mod1tg","Show/Hide fit 1", value=TRUE),
-         checkboxInput("mod2tg","Show/Hide fit 2", value=TRUE),
-         checkboxInput("mod3tg","Show/Hide fit 3", value=TRUE)
-         # submitButton()
+         sliderInput("ptalpha","%Point ocapacity:", min=10, max=100, value=50, step=1),
+         sliderInput("lnwt","Graph line width", min=0.5, max=3, value=1.25, step=0.05),
+         checkboxInput("mod1tg","Show/Hide black linear fit", value=TRUE),
+         checkboxInput("mod2tg","Show/Hide red polynomial fit", value=TRUE),
+         checkboxInput("mod3tg","Show/Hide blue spline fit", value=TRUE)
     ),
     
-    # Show a plot of the generated distribution
+    # Generate plot and predicted values
     mainPanel(
-         plotlyOutput("plot"),
-         "Hola",
-         textOutput("tout1"),
-         textOutput("tout2"),
-         textOutput("tout3")
+         plotlyOutput("plot", width = "100%", height = "200%"),
+         
+         h3(textOutput("tout0")),
+         h4(textOutput("tout1")),
+         h4(textOutput("tout2")),
+         h4(textOutput("tout3")),
+         p("Note conversions to metric are approximate: 1 mph = 1.6 km/h; 1 ft = 0.3 m.")
     )
   )
 ))
